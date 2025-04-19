d4a2dfb0a48c195cfbdbe76971b10493287d0739
Verious Smith III <verious.smith@philoveracity.com>
2025-04-18 23:28:12 -0700

    Initial VeriDX commit.  Setting things in order.

---
+++ wp-content/plugins/veridx-core/veridx-core.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     VeriDX Core
+ * Plugin URI:      https://veridx.com
+ * Description:     VeriDX Core
+ * Author:          VeriDX
+ * Author URI:      https://veridx.com
+ * Text Domain:     vdx
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Veridx_Core
+ */
+
+// Your code starts here.
